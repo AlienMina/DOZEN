29b4dc3a3e9a622ac3d28de72693cfb90d3bd19c
软泥怪的大裤衩 <alienminawisa@outlook.com>
2018-03-16 01:39:21 +0800

    在寻路上做了一些尝试，不过还没有成功，只做出了一些奇怪的雏形效果

---
+++ Assets/scripts/getMouse2D.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getMouse2D : MonoBehaviour {
+    Vector3 screenPosition;
+    Vector3 mousePositionOnScreen;
+    Vector3 mousePositionInWorld;
+    Vector2 mouseXY;
+    PlayMakerFSM fsm;
+
+    private void Start()
+    {
+        fsm = GameObject.Find("SceneFSMs").GetComponent<PlayMakerFSM>();
+    }
+
+    void Update()
+    {
+        screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+        mousePositionOnScreen = Input.mousePosition;
+        mousePositionOnScreen.z = screenPosition.z;
+        mousePositionInWorld = Camera.main.ScreenToWorldPoint(mousePositionOnScreen);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(mousePositionInWorld);
+            //Instantiate(Soldier, mousePositionInWorld, Quaternion.identity);
+
+        }
+
+    }
+
+    public void getMouse() {
+
+        screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+        mousePositionOnScreen = Input.mousePosition;
+        mousePositionOnScreen.z = screenPosition.z;
+        mousePositionInWorld = Camera.main.ScreenToWorldPoint(mousePositionOnScreen);
+
+        mouseXY.x = mousePositionInWorld.x;
+        mouseXY.y = mousePositionInWorld.y;
+        fsm.FsmVariables.GetFsmFloat("mouseX").Value = mouseXY.x;
+        fsm.FsmVariables.GetFsmFloat("mouseY").Value = mouseXY.y;
+    }
+}
