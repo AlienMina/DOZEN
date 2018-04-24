671b72ea18ae8615f2609bb1eba76946325f511b
软泥怪的大裤衩 <alienminawisa@outlook.com>
2018-04-24 23:26:07 +0800

    重置收集品

---
+++ Assets/scripts/collectionLv1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectionLv1 : MonoBehaviour {
+
+    public bool Item1=false;
+    public bool Item2 = false;
+    public bool Memory1 = false;
+    public bool Memory2 = false;
+    public bool Key = false;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
