9f2dcc86b1d7bfd0f0f636ce26c39387ccddce10
软泥怪的大裤衩 <alienminawisa@outlook.com>
2018-04-25 22:56:04 +0800

    做了一点小的调整

---
+++ Assets/scripts/NewBehaviourScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    bool isTalked = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)//碰撞触发对话的示例
+    {
+        if (collision.tag == "Player")//如果碰到了角色的时候
+        {
+            if (!isTalked)
+            {
+                this.gameObject.GetComponent<dialog>().startDialog();
+                isTalked = true;
+            }
+        }
+    }
+
+    }
