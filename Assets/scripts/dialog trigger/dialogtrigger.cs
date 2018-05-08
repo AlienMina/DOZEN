09a05e1008167c3774a8007b63c8231a03e2ddd4
Kagess-PC\四葉影 <kagess@qq.com>
2018-05-08 18:40:14 +0800

    第一章对话初步添加，npc对话框暂用小精灵对话框代替

---
+++ Assets/scripts/dialog trigger/dialogtrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class dialogtrigger : MonoBehaviour {
+    public level1 level1Items;
+
+    public dialog Dialog;
+
+    public NavMeshAgent agent;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Dialog.startDialog();
+            this.gameObject.SetActive(false);
+        }
+    }
+    }
