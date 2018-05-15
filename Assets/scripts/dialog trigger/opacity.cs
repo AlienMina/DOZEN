07eab72e9f05362ac273b22cc0a72d23869adb51
Kagess-PC\四葉影 <kagess@qq.com>
2018-05-15 23:47:34 +0800

    加了一点点东西。（只有一个报表的出现

---
+++ Assets/scripts/dialog trigger/opacity.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class opacity : MonoBehaviour
+{
+    public dialog Dialog;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "machElf")
+        {
+			GetComponent<SpriteRenderer>().enabled = true;
+            Dialog.startDialog();
+            GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(2);
+        this.gameObject.SetActive(false);
+    }
+}
