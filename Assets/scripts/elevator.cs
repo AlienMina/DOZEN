866d06f134ac0d701c07fed6aa92f621f2ca42ad
软泥怪的大裤衩 <alienminawisa@outlook.com>
2018-05-22 23:01:49 +0800

    升降梯v1.0

--- Assets/scripts/elevator.cs
+++ Assets/scripts/elevator.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class elevator : MonoBehaviour {
+public class elevator : MonoBehaviour
+{
 
     public GameCon gameCon;//公共开关的调用
     public GameObject elevatorAnim;//升降梯
     public GameObject player;
-    
+
 
     public GameObject upperPoint;//电梯上方的目标点
     public GameObject upperEnd;//在上方时的下电梯的位置
@@ -24,19 +25,24 @@ public class elevator : MonoBehaviour {
     bool elevatorMoving = false;//电梯是否在移动
     bool readyToLeaveElevator = false;//是否已经要离开电梯
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         if (isDown)
         {
             elevatorDownUpdate();
         }
-
-	}
+        if (isUp)
+        {
+            elevatorUpUpdate();
+        }
+    }
     /// <summary>
     /// 电梯下行
     /// </summary>
@@ -48,7 +54,7 @@ public class elevator : MonoBehaviour {
         Debug.Log(Mathf.Abs(player.GetComponent<NavMeshAgent>().remainingDistance));
         //走到平台上
         player.GetComponent<NavMeshAgent>().destination = upperPoint.transform.position;
-        
+
     }
     void elevatorDownUpdate()
     {
@@ -57,7 +63,7 @@ public class elevator : MonoBehaviour {
         {
             player.GetComponent<NavMeshAgent>().destination = lowerEnd.transform.position;
             //如果已经到达了目标点
-            if (Mathf.Abs(player.GetComponent<NavMeshAgent>().remainingDistance) < between)
+            if (Mathf.Abs(player.transform.position.x - lowerEnd.transform.position.x) < between)
             {
                 gameCon.isElevator = false;
                 isDown = false;
@@ -88,6 +94,59 @@ public class elevator : MonoBehaviour {
                 gameCon.moveable = false;
                 Debug.Log("moveable false");
                 isDown = true;
+            }
+        }
+    }
+
+    public void setElevatorUp()
+    {
+        //已经上电梯了，点击也点不走了
+        gameCon.isElevator = true;
+        isUp = true;
+        Debug.Log(Mathf.Abs(player.GetComponent<NavMeshAgent>().remainingDistance));
+        //走到平台上
+        player.GetComponent<NavMeshAgent>().destination = lowerPoint.transform.position;
+
+    }
+
+    void elevatorUpUpdate()
+    {
+        //如果正在准备离开
+        if (readyToLeaveElevator)
+        {
+            player.GetComponent<NavMeshAgent>().destination = upperEnd.transform.position;
+            //如果已经到达了目标点
+            if (Mathf.Abs(player.transform.position.x - upperEnd.transform.position.x) < between)
+            {
+                gameCon.isElevator = false;
+                isUp = false;
+                readyToLeaveElevator = false;
+                Debug.Log("finish elevator");
+            }
+        }
+        else if (!gameCon.moveable)//如果人物不能播放动画
+        {
+            player.GetComponent<NavMeshAgent>().destination = upperPoint.transform.position;
+            //移动电梯……
+            elevatorAnim.transform.position = player.transform.position;
+            //如果已经到达了目标点
+            if (Mathf.Abs(player.transform.position.y - upperPoint.transform.position.y) < between)
+            {
+                Debug.Log("moveable: " + gameCon.moveable);
+                gameCon.moveable = true;
+                Debug.Log("moveable: " + gameCon.moveable);
+                readyToLeaveElevator = true;
+            }
+        }
+        else
+        {
+            if (Mathf.Abs(player.transform.position.x - lowerPoint.transform.position.x) < between)
+            //if (Mathf.Abs(player.GetComponent<NavMeshAgent>().remainingDistance) < between)
+            {
+                //禁止播放移动动画
+                gameCon.moveable = false;
+                Debug.Log("moveable false");
+                isUp = true;
             }
         }
     }
