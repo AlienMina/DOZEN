32010aaea7a48fee3ae230ab09f2ec7cd725be31
软泥怪的大裤衩 <alienminawisa@outlook.com>
2018-06-14 19:01:35 +0800

    吊灯的效果相关

---
+++ Assets/scripts/brokenTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class brokenTrigger : MonoBehaviour {
+
+    public brokenChandelierQTE QTE;
+    public GameObject UI;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            QTE.isThere = true;
+            UI.SetActive(true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            QTE.isThere = false;
+            UI.SetActive(false);
+        }
+    }
+}
