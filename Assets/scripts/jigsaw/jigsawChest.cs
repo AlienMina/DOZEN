37b107c882657822b76ec07d9144f3f70d117088
软泥怪的大裤衩 <alienminawisa@outlook.com>
2018-06-15 13:17:53 +0800

    拼图的触发写完了，但是完成的部分还没有接出去

---
+++ Assets/scripts/jigsaw/jigsawChest.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jigsawChest : MonoBehaviour {
+
+    public GameObject ui;
+    public GameObject lostPiece;
+    public dialog dia;
+
+    [HideInInspector] public bool havePiece = false;
+    [HideInInspector] public bool chestSeen = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (!chestSeen)//如果是第一次遇见箱子
+            {
+                chestSeen = true;
+                dia.startDialog();
+            }
+            else
+            {
+                if (havePiece)
+                {
+                    lostPiece.SetActive(true);
+                    ui.SetActive(true);
+                }
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "player")
+        {
+            ui.SetActive(false);
+        }
+    }
+}
